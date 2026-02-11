149793b046f003e04930d0ed4d458f09c19ecd65
Swastik <swastik.patro02@gmail.com>
2026-02-11 20:12:15 +0530

    made a node

--- src/agents.ts/amaan.ts
+++ src/agents.ts/amaan.ts
@@ -4,25 +4,25 @@ import {getMarketContext} from '../lib/memory'
 import {z} from 'zod'
 import type { sharkTankState } from "../graph/state"
 
-const asneerResponseSchema = z.object({
+const amaanResponseSchema = z.object({
   dialouge: z.string().describe("This is the response from asneer about the pitch"),
   isOut: z.boolean().describe("This is true if asneer is not interested in the pitch"),
   isInvest: z.boolean().describe("This is true if asneer is interested in the pitch"),
     roast:z.string().describe('This is the data which asneer uses to roast the pitch')
 })
 
-const asneerNode = async (state: typeof sharkTankState.State) =>{
+const amaanNode = async (state: typeof sharkTankState.State) =>{
   const dataContext = await getMarketContext(state.pitch)
 
   //vercel ai sdk to generate responsse from llm
   const result = await generateText({
     model: groq('gemma2-9b-it'),
-    prompt: `You are Ashneer Grover from Shark Tank India - known for being brutally honest, direct, and not afraid to roast bad ideas.Pitch: ${state.pitch},Market Context: ${dataContext}
+    prompt: `You are Amaan Gupta from Shark Tank India -Pitch: ${state.pitch},Market Context: ${dataContext}
 
-    Analyze this pitch and respond in Ashneer's characteristic style. Be honest about whether you're interested or not, and don't hold back on your opinions.`,
+    Analyze this pitch and respond in Amaan's characteristic style. Be honest about whether you're interested or not.`,
   })
 
   return {
-    asneerResponse: result.text
+    amaanResponse: result.text
   }
 }
